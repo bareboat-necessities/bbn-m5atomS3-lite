131f7eda6fd38ca89e9d80f88a6b691b02ea436d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-10-01 12:47:23 -0400

    Create bbn_m5atomS3_lite_i2c_scan.ino

---
+++ bbn_m5atomS3_lite_i2c_scan/bbn_m5atomS3_lite_i2c_scan.ino
@@ -0,0 +1,43 @@
+#include <M5AtomS3.h>
+#include <M5Unified.h>
+#include <Wire.h>
+
+void setup() {
+  auto cfg = M5.config();
+  AtomS3.begin(cfg);
+  Serial.begin(9600);
+  Wire.begin();
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+}
+
+void loop() {
+ delay(3);
+}
