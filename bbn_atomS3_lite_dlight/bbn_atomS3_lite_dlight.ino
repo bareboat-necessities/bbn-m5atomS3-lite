41b4566224009bf69284d075c3d89648e63c5abb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-10-06 14:08:57 -0400

    Create bbn_atomS3_lite_dlight.ino

---
+++ bbn_atomS3_lite_dlight/bbn_atomS3_lite_dlight.ino
@@ -0,0 +1,29 @@
+#include <M5AtomS3.h>
+#include <M5Unified.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <M5_DLight.h>
+
+M5_DLight sensor;
+
+void setup() {
+  auto cfg = M5.config();
+  AtomS3.begin(cfg);
+  Wire.begin();
+  Serial.begin(115200);
+  sensor.begin();
+
+  // CONTINUOUSLY_H_RESOLUTION_MODE
+  // CONTINUOUSLY_H_RESOLUTION_MODE2
+  // CONTINUOUSLY_L_RESOLUTION_MODE
+  // ONE_TIME_H_RESOLUTION_MODE
+  // ONE_TIME_H_RESOLUTION_MODE2
+  // ONE_TIME_L_RESOLUTION_MODE
+  sensor.setMode(CONTINUOUSLY_L_RESOLUTION_MODE);
+}
+
+void loop() {
+  uint16_t lux = sensor.getLUX();
+  Serial.printf("lux: %d\n", lux);
+  delay(200);
+}
