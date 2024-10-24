e3255ff1d2ab87a00420ad6dabaf0fc20ad3b8ae
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-10-23 20:26:32 -0400

    Update bbn_m5atomS3_lite_DS18B20.ino

--- bbn_m5atomS3_lite_DS18B20/bbn_m5atomS3_lite_DS18B20.ino
+++ bbn_m5atomS3_lite_DS18B20/bbn_m5atomS3_lite_DS18B20.ino
@@ -3,7 +3,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS G8
+#define ONE_WIRE_BUS G8          // Pin to which 1wire bus is connected to esp32
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
@@ -16,12 +16,26 @@ void setup() {
   Serial.printf("Sensors found %d\n", count);
 }
 
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
 void loop() {
   int count = sensors.getDeviceCount();
   if (count > 0) {
     sensors.requestTemperatures();
     for (int i = 0; i < count; i++) {
-      Serial.printf("Temp %.2f\n", sensors.getTempCByIndex(i));
+      DeviceAddress addr;
+      if (!sensors.getAddress(addr, i)) {
+        Serial.println("Unable to find address for Device");
+      } else {
+        printAddress(addr);
+      }
+      Serial.printf(" Temp %.2f\n", sensors.getTempCByIndex(i));
     }
   }
   delay(1000);
