964a67242c807d23c254f716ea1768f2da538bd4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-10-29 23:39:02 -0400

    Update CQRobotTDS.h

--- bbn_m5atomS3_lite_TDS_CQRSENTDS01/CQRobotTDS.h
+++ bbn_m5atomS3_lite_TDS_CQRSENTDS01/CQRobotTDS.h
@@ -1,6 +1,15 @@
 #ifndef CQROBOT_H
 #define CQROBOT_H
 
+/*
+
+  The ideal TDS level for drinking water is 300–500 ppm, while 500 ppm 
+  is the maximum recommended by the EPA. Water with a TDS level above 1,000 ppm 
+  is not considered safe to drink, and levels above 2,000 ppm may 
+  require a filtration system.
+
+*/
+
 #define CQROBOT_SCOUNT  30
 
 class CQRobotTDS {
