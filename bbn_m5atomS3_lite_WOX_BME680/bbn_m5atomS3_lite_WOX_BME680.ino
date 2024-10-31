03442a3962a2010aacdf9709cea4c3f9440dd0dc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-10-30 23:14:28 -0400

    Update bbn_m5atomS3_lite_WOX_BME680.ino

--- bbn_m5atomS3_lite_WOX_BME680/bbn_m5atomS3_lite_WOX_BME680.ino
+++ bbn_m5atomS3_lite_WOX_BME680/bbn_m5atomS3_lite_WOX_BME680.ino
@@ -1,62 +1,38 @@
 #include <M5AtomS3.h>
-#include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_SGP30.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
 #include "NmeaXDR.h"
 #include "NmeaChecksum.h"
 
-Adafruit_SGP30 sgp;
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme(&Wire); // I2C
 
 void setup() {
   auto cfg = M5.config();
   AtomS3.begin(cfg);
-  Serial.begin(4800);
   Wire.begin();
   Wire.setClock(100000);
   delay(10);
-  if (!sgp.begin(&Wire, true)) {  // Init the sensor.
-    while (1) ;
-  }
-  //Serial.print("Found SGP30 serial #");
-  //Serial.print(sgp.serialnumber[0], HEX);
-  //Serial.print(sgp.serialnumber[1], HEX);
-  //Serial.println(sgp.serialnumber[2], HEX);
+  Serial.begin(4800);
+  while (!bme.begin()) {}
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
-int counter = 0;
 void loop() {
-  // If you have a temperature / humidity sensor, you can set the absolute humidity 
-  // to enable the humditiy compensation for the air quality signals
-
-  //float temperature = 22.1; // [°C]
-  //float humidity = 45.2; // [%RH]
-  //sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
-
-  // Commands the sensor to take a single eCO2/VOC measurement.
-  if (!sgp.IAQmeasure()) { 
-    // Measurement failed
+  if (!bme.performReading()) {
     delay(4);
     return;
   }
-  gen_nmea0183_xdr("$BBXDR,X,%.0f,,TVOC", sgp.TVOC);        // ppb
-  gen_nmea0183_xdr("$BBXDR,X,%.0f,,eCO2", sgp.eCO2);        // ppm
-
-  if (! sgp.IAQmeasureRaw()) {
-    // Raw Measurement failed
-    delay(4);
-    return;
-  }
-  gen_nmea0183_xdr("$BBXDR,X,%.0f,,rawH2", sgp.rawH2);       
-  gen_nmea0183_xdr("$BBXDR,X,%.0f,,rawEthanol", sgp.rawEthanol);       
-
-  delay(1000);
-
-  counter++;
-  if (counter == 30) {
-    counter = 0;
-    uint16_t TVOC_base, eCO2_base;
-    if (! sgp.getIAQBaseline(&eCO2_base, &TVOC_base)) {
-      return;
-    }
-  }
+  gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_BME680", bme.temperature);             // C
+  gen_nmea0183_xdr("$BBXDR,P,%.2f,P,PRES_BME680", bme.pressure);                // Pa
+  gen_nmea0183_xdr("$BBXDR,H,%.2f,P,HUMI_BME680", bme.humidity);                // %
+  gen_nmea0183_xdr("$BBXDR,H,%.2f,,GASR_BME680", bme.gas_resistance / 1000.0);  // KOhms
+  delay(5000);
 }
