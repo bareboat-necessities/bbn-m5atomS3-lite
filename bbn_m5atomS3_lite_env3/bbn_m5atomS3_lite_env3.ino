3017f361b2b32786ad0f696638984803b7a77fe7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-11-05 20:21:52 -0500

    Update bbn_m5atomS3_lite_env3.ino

--- bbn_m5atomS3_lite_env3/bbn_m5atomS3_lite_env3.ino
+++ bbn_m5atomS3_lite_env3/bbn_m5atomS3_lite_env3.ino
@@ -14,13 +14,13 @@ void setup() {
   Serial.begin(4800);
 
   int i = 0;
-  while (!qmp6988.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, G2, G1, 100000U) && i < 10) {
+  while (!qmp6988.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, G2, G1, 100000U) && i < 3) {
     gen_nmea0183_msg("$BBTXT,01,01,01,Sensor not found type=%s", "QMP6988");
     delay(4);
     i++;
   }
   i = 0;
-  while (!sht3x.begin(&Wire, SHT3X_I2C_ADDR, G2, G1, 100000U) && i < 10) {
+  while (!sht3x.begin(&Wire, SHT3X_I2C_ADDR, G2, G1, 100000U) && i < 3) {
     gen_nmea0183_msg("$BBTXT,01,01,01,Sensor not found type=%s", "SHT3X");
     delay(4);
     i++;
