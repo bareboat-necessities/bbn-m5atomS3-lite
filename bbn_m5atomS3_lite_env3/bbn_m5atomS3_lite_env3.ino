388e0df5e40e53086b586e004b9a5d1f47245ead
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-11-05 20:08:39 -0500

    Update bbn_m5atomS3_lite_env3.ino

--- bbn_m5atomS3_lite_env3/bbn_m5atomS3_lite_env3.ino
+++ bbn_m5atomS3_lite_env3/bbn_m5atomS3_lite_env3.ino
@@ -1,5 +1,4 @@
 #include <M5AtomS3.h>
-#include <Arduino.h>
 #include <M5UnitENV.h>
 #include <Wire.h>
 #include "NmeaXDR.h"
@@ -13,19 +12,25 @@ void setup() {
   AtomS3.begin(cfg);
   Wire.begin();
   Serial.begin(4800);
-  
-  if (!qmp6988.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, G2, G1, 100000U)) {
+
+  int i = 0;
+  while (!qmp6988.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, G2, G1, 100000U) && i < 10) {
     gen_nmea0183_msg("$BBTXT,01,01,01,Sensor not found type=%s", "QMP6988");
+    delay(4);
+    i++;
   }
-  if (!sht3x.begin(&Wire, SHT3X_I2C_ADDR, G2, G1, 100000U)) {
+  i = 0;
+  while (!sht3x.begin(&Wire, SHT3X_I2C_ADDR, G2, G1, 100000U) && i < 10) {
     gen_nmea0183_msg("$BBTXT,01,01,01,Sensor not found type=%s", "SHT3X");
+    delay(4);
+    i++;
   }
 }
 
 void loop() {
   if (sht3x.update()) {
     gen_nmea0183_xdr("$BBXDR,H,%.2f,P,HUMI_SHT3X", sht3x.humidity);   // %
-    gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_SHT3X", sht3x.cTemp);    // C
+    gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_SHT3X", sht3x.cTemp);      // C
   }
   if (qmp6988.update()) {
     gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_QMP6988", qmp6988.cTemp);        // C
