a85a18ba2900d01dae0d81528d339fa1ddd6e516
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-19 10:21:19 -0500

    Update MODULE_4_20MA.h

--- bbn_m5atomS3_lite_ain_4_20ma/MODULE_4_20MA.h
+++ bbn_m5atomS3_lite_ain_4_20ma/MODULE_4_20MA.h
@@ -1,6 +1,9 @@
 #ifndef __MODULE_4_20MA_H
 #define __MODULE_4_20MA_H
 
+#include <M5Unified.h>
+#include <Wire.h>
+
 #define MODULE_4_20MA_ADDR          0x55
 #define MODULE_4_20MA_ADC_12BIT_REG 0x00
 #define MODULE_4_20MA_CURRENT_REG   0x20
