a9664459ab2333bde998f7b5c38b1d679fcdd909
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-18 23:06:49 -0500

    Update bbn_m5atomS3_lite_angle_unit.ino

--- bbn_m5atomS3_lite_angle_unit/bbn_m5atomS3_lite_angle_unit.ino
+++ bbn_m5atomS3_lite_angle_unit/bbn_m5atomS3_lite_angle_unit.ino
@@ -1,4 +1,4 @@
-#include <M5AtomS3.h>
+#include <M5Unified.h>
 #include "NmeaXDR.h"
 
 struct AngleInput {
@@ -26,7 +26,7 @@ AngleInput angle_input;
 
 void setup() {
   auto cfg = M5.config();
-  AtomS3.begin(cfg);
+  M5.begin(cfg);
   Serial.begin(4800);
   pinMode(angle_input.potentiometerPin, INPUT);  // Sets the specified pin to input mode.
 }
