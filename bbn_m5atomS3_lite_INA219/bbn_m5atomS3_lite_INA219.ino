0be999287dbfef0d462fc3d165baf04371d2fbd2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-18 22:41:44 -0500

    Update bbn_m5atomS3_lite_INA219.ino

--- bbn_m5atomS3_lite_INA219/bbn_m5atomS3_lite_INA219.ino
+++ bbn_m5atomS3_lite_INA219/bbn_m5atomS3_lite_INA219.ino
@@ -1,4 +1,4 @@
-#include <M5AtomS3.h>
+#include <M5Unified.h>
 #include <Adafruit_INA219.h>
 
 #include "NmeaXDR.h"
@@ -13,7 +13,7 @@ unsigned long timeout = 0;
 
 void setup()   {
   auto cfg = M5.config();
-  AtomS3.begin(cfg);
+  M5.begin(cfg);
   Serial.begin(4800);
   Wire.begin(G2, G1, 100000UL);
   if (!ina219_0.begin()) {
